4418496f205a0bfa3853cd2e3ca82911cea6ec43
Plant Lady <codeplantsbylouie@gmail.com>
2019-04-15 13:20:08 -0400

    js is now DRY

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,184 +1,226 @@
+// Execute this code when the DOM has fully loaded.
 $(document).ready(function() {
+  // VARIABLE DECLARATION
+  // ===================================================================
 
-  // DEFAULT AREA
-  // create an arry of objects; default character list; wil not be the code that i mess with while palying the game; keep it on its own; then when we restart, we use this data to feed inot the html page; has all of the defaults
-  var characterList = [
-    {
-      name: 'kyle katarn',
+  // Creating an object to hold our characters.
+  var characters = {
+    "Obi-Wan Kenobi": {
+      name: "Obi-Wan Kenobi",
       health: 120,
       attack: 8,
-      counter: 15,
-      defaultAttack: 8, // base attack; added on every turn
-      pic: "../../images/kyle-katarn.jpeg"
+      imageUrl: "assets/images/obi-wan.jpg",
+      enemyAttackBack: 15
     },
-    {
-      name: 'lando calrissian',
+    "Luke Skywalker": {
+      name: "Luke Skywalker",
       health: 100,
       attack: 14,
-      counter: 5,
-      defaultAttack: 14,
-      pic: "../../images/lando.jpeg"
+      imageUrl: "assets/images/luke-skywalker.jpg",
+      enemyAttackBack: 5
     },
-    {
-      name: 'ig-88',
+    "Darth Sidious": {
+      name: "Darth Sidious",
       health: 150,
       attack: 8,
-      counter: 20,
-      defaultAttack: 8,
-      pic: "../../images/ig-88.jpeg"
+      imageUrl: "assets/images/darth-sidious.png",
+      enemyAttackBack: 20
     },
-    {
-      name: 'Boba Fett',
+    "Darth Maul": {
+      name: "Darth Maul",
       health: 180,
       attack: 7,
-      counter: 25,
-      defaultAttack: 7,
-      pic: "../../images/boba-fett.jpeg"
-    },
-  ];
-
-  // VARIABLE FOR PLAYING THE GAME
-  var gameRoster = []; // be the variable to keep track what's happening in the game
-  var player = {}; // empty object for player and the computer
-  var computer = {}; // hold onto each character's info when we select them; so that player will be in the object
-  var playerPicked = false; // setting some flags; when a selected a player and the computer too
-  var computerPicked = false;
-  var gameRunning = false; // game running flag; turn off the buttons; don't want the characters to keep playing
-
-  // NEW/RESET FUNCTION
-  function newGame() {
-    gameRoster = [...characterList]; // grab character list with this spread operator --> and spread it across a new array; meaning is: in js you ajave 2 diff types of data --> primitive data types(strings, boolean, integers); works with arrays and objects: it says let's take the contents of this array/ogject and let's make a new array/object; they create an entire new piece of info from it
-    console.log(characterList);
-    player = {}; // zero out player
-    computer = {}; // zero out computer
-    gameRunning = true;
-    playerPicked = false;
-    computerPicked = false;
-
-    $("#character-grid").hide();
-    printCharacters(gameRoster);
-  }
-
-  // PRINT CHARACTER LIST
-  function printCharacters(characterArray) { // taking character array as an argument
-    $("#character-list").empty(); // take that div and empty it out; make sure not picking same character over and over again
-
-    // check to see if the character array.length is zero
-    if(characterArray.length === 0) {
-      $("#character-list").text("No characters left");
-      return false; // return false --> that way won't try to print anything
+      imageUrl: "assets/images/darth-maul.jpg",
+      enemyAttackBack: 25
     }
+  };
 
-    // useing a forEach loop --> an array method; only works on arrays; for each element in the array, lets run a function
-    characterArray.forEach(function(characterInfo, i) { // it's like a for loop but it's really specific to arrays; at each iteration it's going to run this function (characterInfo) and there is the iterator 'i'
-    // saying: for each character in characterArray... 
-    // characterInfo represent what the character is at that iteration of the loop
-        // Character Div
-        // for each character we will create a character and addClass
-        var character = $("<div class='character'>");
-        // give character a background css property
-        character.css({"background-image": "url(" + characterInfo.pic + ")"}).attr({"data-id": i});
-        
-        // need to create a div that will hold on to the statuses
-        var charStats = $("<div>");
-        charStats.addClass("character-stats").html(`<h4>${characterInfo.name}</h4>HP: ${characterInfo.health} <br>Attack: ${characterInfo.attack}<br>Counter: ${characterInfo.counter}`).appendTo(character);
-        // template literals; wrap a string in back ticks, can write any valid code you want (not compatible with internet explorere); 
+  // Will be populated when the player selects a character.
+  var attacker;
+  // Populated with all the characters the player didn't select.
+  var combatants = [];
+  // Will be populated when the player chooses an opponent.
+  var defender;
+  // Will keep track of turns during combat. Used for calculating player damage.
+  var turnCounter = 1;
+  // Tracks number of defeated opponents.
+  var killCount = 0;
 
-        $("#character-list").append(character)
+  // FUNCTIONS
+  // ===================================================================
 
-    })
-  }
+  // This function will render a character card to the page.
+  // The character rendered, the area they are rendered to, and their status is determined by the arguments passed in.
+  var renderCharacter = function(character, renderArea) {
+    // This block of code builds the character card, and renders it to the page.
+    var charDiv = $("<div class='character' data-name='" + character.name + "'>");
+    var charName = $("<div class='character-name'>").text(character.name);
+    var charImage = $("<img alt='image' class='character-image'>").attr("src", character.imageUrl);
+    var charHealth = $("<div class='character-health'>").text(character.health);
+    charDiv.append(charName).append(charImage).append(charHealth);
+    $(renderArea).append(charDiv);
+  };
 
-  // ADD PLAYER TO ACTIVE GAME AREA
-  // function that takes in 4 diff arguments,
-  function activatePlayer(playerInfo, playerPosition, playerStats, playerTitle) {
-    console.log(playerInfo, playerPosition);
+  // this function will load all the characters into the character section to be selected
+  var initializeGame = function() {
+    // Loop through the characters object and call the renderCharacter function on each character to render their card.
+    for (var key in characters) {
+      renderCharacter(characters[key], "#characters-section");
+    }
+  };
 
-    $(playerPosition).css({
-      "background-image": "url(" + playerInfo.pic + ")"
+  // remember to run the function here
+  initializeGame();
+
+  // This function handles updating the selected player or the current defender. If there is no selected player/defender this
+  // function will also place the character based on the areaRender chosen (e.g. #selected-character or #defender)
+  var updateCharacter = function(charObj, areaRender) {
+    // First we empty the area so that we can re-render the new object
+    $(areaRender).empty();
+    renderCharacter(charObj, areaRender);
+  };
+
+  // This function will render the available-to-attack enemies. This should be run once after a character has been selected
+  var renderEnemies = function(enemyArr) {
+    for (var i = 0; i < enemyArr.length; i++) {
+      renderCharacter(enemyArr[i], "#available-to-attack-section");
+    }
+  };
+
+  // Function to handle rendering game messages.
+  var renderMessage = function(message) {
+    // Builds the message and appends it to the page.
+    var gameMessageSet = $("#game-message");
+    var newMessage = $("<div>").text(message);
+    gameMessageSet.append(newMessage);
+  };
+
+  // Function which handles restarting the game after victory or defeat.
+  var restartGame = function(resultMessage) {
+    // When the 'Restart' button is clicked, reload the page.
+    var restart = $("<button>Restart</button>").click(function() {
+      location.reload();
     });
 
-    // print out player stats while using template literals
-    $(playerStats)
-      .html(`<h4>${playerInfo.name} - ${playerTitle}</h4>HP: ${playerInfo.health}<br>Attack: ${playerInfo.attack}<br>Counter: ${playerInfo.counter}`)
-      .removeClass("defeated");
-  }
+    // Build div that will display the victory/defeat message.
+    var gameState = $("<div>").text(resultMessage);
 
-  // SELECT A CHARACTER
-  // make an event listener; bound it to the document, specifically to anything with the class of character
-  $(document).on("click", ".character", function() {
-    // get character id so we can pull it out of the gameRoster array by its index
-    var playerId = $(this).attr("data-id");
+    // Render the restart button and victory/defeat message to the page.
+    $("body").append(gameState);
+    $("body").append(restart);
+  };
 
-    // if you click on a character, 
-    if (!playerPicked && gameRunning) {
-      playerPicked = true;
-      player = {...gameRoster[playerId]}; // by using spread operator, have playerId, reps index of gameRoster, grab the player by index in gameRoster;
-      $("#character-grid").show(); // show the character grid bc we're about to show a character to it
-      gameRoster.splice(playerId, 1); // splice; first arg is what index to we start at and then how many from that index should we be concerned with;
-      printCharacters(gameRoster);
-      activatePlayer(player, "#player", "#player-stats", "Attacker"); // send all this infor to a diff function activatePlayer; send id of div where info will go
-    } 
-    else if (!computerPicked && gameRunning) {
-      computerPicked = true;
-      computer = {...gameRoster[playerId]};
-      gameRoster.splice(playerId, 1);
-      printCharacters(gameRoster);
-      activatePlayer(computer, "#computer", "#computer-stats", "Defender");
-    } 
-    else {
-      console.log("Both players are active");
-    }
-  });
+  // Function to clear the game message section
+  var clearMessage = function() {
+    var gameMessage = $("#game-message");
 
-  function attack() {
-    // Player Attacks CPU
-    computer.health -= player.attack;
-    // Player's attack increases
-    player.attack += player.defaultAttack;
+    gameMessage.text("");
+  };
 
-    // check to see if defender has lost
-    if (computer.health <=0) {
-      computerPicked = false; // reopening that click event to add a anew defender
-      $("#computer-stats").html(`<h3>DEFEATED!</h3><h4>${computer.name} - Defender</h4>HP: ${computer.health} <br>Attack: ${computer.attack} <br>Counter: ${computer.counter}`).addClass("defeated");
+  // ===================================================================
 
-      // check to see if any more characters are left
-      if (gameRoster.length === 0) {
-        gameRunning = false;
+  // On click event for selecting our character.
+  $("#characters-section").on("click", ".character", function() {
+    // Saving the clicked character's name.
+    var name = $(this).attr("data-name");
+
+    // If a player character has not yet been chosen...
+    if (!attacker) {
+      // We populate attacker with the selected character's information.
+      attacker = characters[name];
+      // We then loop through the remaining characters and push them to the combatants array.
+      for (var key in characters) {
+        if (key !== name) {
+          combatants.push(characters[key]);
+        }
       }
-      return false;
-    }
 
-    // CPU Attacks Player
-    player.health -= computer.counter;
+      // Hide the character select div.
+      $("#characters-section").hide();
 
-    $("#player-stats").html(`<h4>${player.name} - Attacker</h4>HP: ${player.health} <br>Attack: ${player.attack}<br>Counter: ${player.counter}`);
-
-    $("#computer-stats").html(`<h4>${computer.name} - Attacker</h4>HP: ${computer.health} <br>Attack: ${computer.attack}<br>Counter: ${computer.counter}`);
-
-    if (player.health <= 0) {
-      playerPicked = false;
-      computerPicked = false;
-      gameRunning = false;
-      $("#player-stats").html(`<h3>DEFEATED!</h3><h4>${player.name} - Defender</h4>HP: ${player.health} <br>Attack: ${player.attack} <br>Counter: ${player.counter}`).addClass("defeated");    
-    }
-  }
-
-  // NEW GAME BUTTON
-  $("#new-game").on("click", newGame);
-
-  // ATTACK BUTTON
-  $("#attack").on("click", function() {
-    // on click we're going to run this function
-    if (playerPicked && computerPicked && gameRunning) {
-      attack();
-    } 
-    else {
-      console.log('You need to pick a player');
+      // Then render our selected character and our combatants.
+      updateCharacter(attacker, "#selected-character");
+      renderEnemies(combatants);
     }
   });
 
-  newGame();
+  // Creates an on click event for each enemy.
+  $("#available-to-attack-section").on("click", ".character", function() {
+    // Saving the opponent's name.
+    var name = $(this).attr("data-name");
 
+    // If there is no defender, the clicked enemy will become the defender.
+    if ($("#defender").children().length === 0) {
+      defender = characters[name];
+      updateCharacter(defender, "#defender");
+
+      // remove element as it will now be a new defender
+      $(this).remove();
+      clearMessage();
+    }
+  });
+
+  // When you click the attack button, run the following game logic...
+  $("#attack-button").on("click", function() {
+    // If there is a defender, combat will occur.
+    if ($("#defender").children().length !== 0) {
+      // Creates messages for our attack and our opponents counter attack.
+      var attackMessage = "You attacked " + defender.name + " for " + attacker.attack * turnCounter + " damage.";
+      var counterAttackMessage = defender.name + " attacked you back for " + defender.enemyAttackBack + " damage.";
+      clearMessage();
+
+      // Reduce defender's health by your attack value.
+      defender.health -= attacker.attack * turnCounter;
+
+      // If the enemy still has health..
+      if (defender.health > 0) {
+        // Render the enemy's updated character card.
+        updateCharacter(defender, "#defender");
+
+        // Render the combat messages.
+        renderMessage(attackMessage);
+        renderMessage(counterAttackMessage);
+
+        // Reduce your health by the opponent's attack value.
+        attacker.health -= defender.enemyAttackBack;
+
+        // Render the player's updated character card.
+        updateCharacter(attacker, "#selected-character");
+
+        // If you have less than zero health the game ends.
+        // We call the restartGame function to allow the user to restart the game and play again.
+        if (attacker.health <= 0) {
+          clearMessage();
+          restartGame("You have been defeated...GAME OVER!!!");
+          $("#attack-button").off("click");
+        }
+      }
+      else {
+        // If the enemy has less than zero health they are defeated.
+        // Remove your opponent's character card.
+        $("#defender").empty();
+
+        var gameStateMessage = "You have defeated " + defender.name + ", you can choose to fight another enemy.";
+        renderMessage(gameStateMessage);
+
+        // Increment your kill count.
+        killCount++;
+
+        // If you have killed all of your opponents you win.
+        // Call the restartGame function to allow the user to restart the game and play again.
+        if (killCount >= combatants.length) {
+          clearMessage();
+          $("#attack-button").off("click");
+          restartGame("You Won!!!! GAME OVER!!!");
+        }
+      }
+      // Increment turn counter. This is used for determining how much damage the player does.
+      turnCounter++;
+    }
+    else {
+      // If there is no defender, render an error message.
+      clearMessage();
+      renderMessage("No enemy here.");
+    }
+  });
 });
-
